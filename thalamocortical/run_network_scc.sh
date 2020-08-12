595bdb7790495822e8d50e8e13ac9c860e700743
Marianne Bezaire <marianne.bezaire@gmail.com>
2020-08-12 15:28:10 -0400

    updated scc code

---
+++ thalamocortical/run_network_scc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#$ -P riseprac
+
+#$ -pe omp 8
+
+#$ -l h_rt=1:00:00
+
+#$ -m ea
+
+#$ -N networkonly
+
+#$ -j y
+#$ -o log_networkonly.qlog
+
+echo "============================================"
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID: $JOB_ID $SGE_TASK_ID"
+echo "============================================"
+
+module purge
+module load python3/3.6.5
+module load gcc/8.1.0
+module load openmpi/3.1.4_gnu-8.1
+module load neuron/7.6.7
+module load gsl/2.5
+module load nest/2.20.0
+
+mpirun -np 8 python network.py
